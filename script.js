87d22f3fb4bea8449a80918637bad69a41e6bde2
Aleksey <sculovorot@mail.ru>
2020-02-24 15:38:38 +0300

    third commit

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),  //полученные данные-строка поэтому поставили унарный плюс чтоб преобразовать в число
+'use strict';
+let money, time;
+function start(){
+    money = +prompt("Ваш бюджет на месяц?", '');  //полученные данные-строка поэтому поставили унарный плюс чтоб преобразовать в число
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
+    //пока что-то не произошло, мы пользователя задолбаем вопросом про бюджет. isNaN возвращает true если попадает туда не цифры
+    while(isNaN(money) || money == '' || money == null) {  //null чтоб пользователь не смог нажать кнопку отмена. Цикл будет продолжаться если один из вариантов выполнится
+        money = prompt("Ваш бюджет на месяц", '');
+    }
+}
+start();
+
 let appData = {
     budget: money, //вводимая переменная
     timeData: time, //вводимая дата
     expenses: {},   // ОБЪЕКТ обязательные расходы
     optionalExpenses: {}, //необязательные расходы
     income: [], //данные с доп доходом
-    savings: false
+    savings: true
 };
 
-// порписываем цикл способ 1
-for (let i = 0; i < 2; i++) {
-   let a = prompt("Bведите обязательную статью расходов", ''),  
-       b = prompt("Во сколько обойдётся", '');
-    // порверяем что входящие данные a и b являются строкой и не нажата кнопка отмены т.е не была равна null != 
-    if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null 
-    //также проверяем что пользователь не оставил строку пустой и не более 50 символов
-    && a != '' && b != '' && a.length < 50) { 
-        console.log("done");  
-        appData.expenses[a] = b;
-    } else {
-
-    }
-};
-
+function chooseExpenses(){
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Bведите обязательную статью расходов", ''),  
+            b = prompt("Во сколько обойдётся", '');
+         // порверяем что входящие данные a и b являются строкой и не нажата кнопка отмены т.е не была равна null != 
+         if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null 
+         //также проверяем что пользователь не оставил строку пустой и не более 50 символов
+         && a != '' && b != '' && a.length < 50) { 
+             console.log("done");  
+             appData.expenses[a] = b;
+         } else {
+             i = i--;
+         }
+     };
+}
+chooseExpenses();
 // порписываем цикл способ 2
 // let i = 0;
 // while(i < 2) {
@@ -38,9 +49,12 @@ for (let i = 0; i < 2; i++) {
 //     }
 // };
 
-//бюджет на 1 день
-appData.moneyPerDay = appData.budget / 30;
-alert("Ежедневный бюджет" + appData.moneyPerDay);
+//определение дневного бюджета
+function detectDayBudget(){
+     appData.moneyPerDay = (appData.budget / 30).toFixed(); //toFixed округляем наше число до целого возвр стр значение
+     alert("Ежедневный бюджет" + appData.moneyPerDay + "руб");
+}
+detectDayBudget();
 
 // Выведем уроввень достатка человека в зависимости от ежедневного бюджета
 if(appData.moneyPerDay < 100) {
@@ -52,3 +66,24 @@ if(appData.moneyPerDay < 100) {
 }else {
     console.log("Произошла ошибка");
 }
+
+//расчитаем накопления с депозита
+function checkSavings(){
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+        appData.monthIncome = save/100/12*percent; //вводим новое св-во 
+        alert("Доход в месяц с Вашего депозита: " + appData.monthIncome);
+    }
+}
+checkSavings();
+
+//определение необязательных расходов
+function chooseOptExpenses(){
+    for (let i = 1; i <= 3; i++) {
+        let c = prompt("Статья необязательных расходов", '');
+    appData.optionalExpenses[i] = c;
+    console.log(appData.optionalExpenses);
+    }
+}
+chooseOptExpenses();
